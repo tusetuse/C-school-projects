b9839c4525e1dd428292a2707b01f6e393a2f931
IvanRonda <ivanronda500@gmail.com>
2024-12-12 08:45:14 +0100

    Chleba s marmeladou 2

--- Zapocet_3/Form1.Designer.cs
+++ Zapocet_3/Form1.Designer.cs
@@ -48,32 +48,36 @@
             this.panelTop.Controls.Add(this.lblIP);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(584, 50);
+            this.panelTop.Size = new System.Drawing.Size(779, 62);
             this.panelTop.TabIndex = 0;
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(280, 10);
+            this.btnConnect.Location = new System.Drawing.Point(373, 12);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(100, 23);
+            this.btnConnect.Size = new System.Drawing.Size(133, 28);
             this.btnConnect.TabIndex = 2;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(120, 12);
+            this.txtIP.Location = new System.Drawing.Point(160, 15);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(150, 20);
+            this.txtIP.Size = new System.Drawing.Size(199, 22);
             this.txtIP.TabIndex = 1;
             // 
             // lblIP
             // 
             this.lblIP.AutoSize = true;
-            this.lblIP.Location = new System.Drawing.Point(12, 15);
+            this.lblIP.Location = new System.Drawing.Point(16, 18);
+            this.lblIP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIP.Name = "lblIP";
-            this.lblIP.Size = new System.Drawing.Size(84, 13);
+            this.lblIP.Size = new System.Drawing.Size(104, 16);
             this.lblIP.TabIndex = 0;
             this.lblIP.Text = "PLC IP Address:";
             // 
@@ -81,25 +85,28 @@
             // 
             this.panelVariables.AutoScroll = true;
             this.panelVariables.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelVariables.Location = new System.Drawing.Point(0, 50);
+            this.panelVariables.Location = new System.Drawing.Point(0, 62);
+            this.panelVariables.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelVariables.Name = "panelVariables";
-            this.panelVariables.Padding = new System.Windows.Forms.Padding(10);
-            this.panelVariables.Size = new System.Drawing.Size(584, 289);
+            this.panelVariables.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.panelVariables.Size = new System.Drawing.Size(779, 356);
             this.panelVariables.TabIndex = 1;
             // 
             // statusStrip
             // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip.Location = new System.Drawing.Point(0, 418);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip.Size = new System.Drawing.Size(779, 26);
             this.statusStrip.TabIndex = 2;
             // 
             // statusLabel
             // 
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(79, 17);
+            this.statusLabel.Size = new System.Drawing.Size(99, 20);
             this.statusLabel.Text = "Disconnected";
             // 
             // updateTimer
@@ -108,13 +115,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ClientSize = new System.Drawing.Size(779, 444);
             this.Controls.Add(this.panelVariables);
             this.Controls.Add(this.panelTop);
             this.Controls.Add(this.statusStrip);
-            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(527, 358);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PLC Monitor";
